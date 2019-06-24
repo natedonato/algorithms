9c02446c503d175d2899fa254f7a56325c1a3fba
Nate Donato <natedonato@gmail.com>
2019-06-24 14:12:41 -0700

    did another problem

---
+++ leetcode/9.js
@@ -0,0 +1,20 @@
+/**
+ * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+ * Easy
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    if(x < 0){ return false;}
+    return(x === reverse(x));
+};
+
+var reverse = function (x) {
+    let reversed = 0;
+    while (x > 0) {
+        let lastDigit = x % 10;
+        reversed = reversed * 10 + lastDigit;
+        x = Math.floor(x / 10);
+    }
+    return reversed;
+};
