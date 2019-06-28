52047427df6416e74bc4e1364c46a155da0f69c5
Nate Donato <natedonato@gmail.com>
2019-06-28 16:02:29 -0700

    start 14

---
+++ leetcode/14.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    let first = strs.unshift();
+    let i = 0;
+    let output = "";
+
+    
+
+};
