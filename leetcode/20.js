75622c595b0ee7f68f755f1fde964b048143e5eb
Nate Donato <natedonato@gmail.com>
2019-06-28 17:11:23 -0700

    finished 20

---
+++ leetcode/20.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var isValid = function (s) {
+    const pairs = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    let stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        char = s[i];
+        if (!pairs[char]) {
+            stack.push(char);
+        }
+        else if (pairs[char] !== stack.pop()) {
+            return false;
+        }
+    }
+    return stack.length === 0;
+};
+
